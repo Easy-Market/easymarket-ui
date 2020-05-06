0a4c894d7ae14e227a827cea9d55a238d9f36f4f
Obinna Kelvin <okereobinna10@gmail.com>
2020-05-06 01:11:02 +0000

    Updates

---
+++ precache-manifest.0a38cc83e6514ba450ad74668c68124c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596172402f06316e4faf31103640b9e6",
+    "url": "/easymarket-ui/index.html"
+  },
+  {
+    "revision": "57e97b32f006a33d1f96",
+    "url": "/easymarket-ui/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "639338284ad2fa426542",
+    "url": "/easymarket-ui/static/css/main.69d5006d.chunk.css"
+  },
+  {
+    "revision": "57e97b32f006a33d1f96",
+    "url": "/easymarket-ui/static/js/2.78b05c08.chunk.js"
+  },
+  {
+    "revision": "639338284ad2fa426542",
+    "url": "/easymarket-ui/static/js/main.afeb3cf0.chunk.js"
+  },
+  {
+    "revision": "c793881ea12eed09f34d",
+    "url": "/easymarket-ui/static/js/runtime-main.c2c66111.js"
+  },
+  {
+    "revision": "af546ce573722addb4406b068256c2f2",
+    "url": "/easymarket-ui/static/media/bg1.af546ce5.jpg"
+  },
+  {
+    "revision": "67a4821460315e3b256a1497c220a268",
+    "url": "/easymarket-ui/static/media/cloud.67a48214.svg"
+  },
+  {
+    "revision": "48a85121304d8510bb849e482b00c51b",
+    "url": "/easymarket-ui/static/media/docs.48a85121.svg"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/easymarket-ui/static/media/google-play-badge.db9b21a1.png"
+  },
+  {
+    "revision": "2ef193934ab2267b9552df374ca074ce",
+    "url": "/easymarket-ui/static/media/hacker.2ef19393.svg"
+  },
+  {
+    "revision": "a393b9c6ad6ce909c371f13fc7b89d4b",
+    "url": "/easymarket-ui/static/media/joy.a393b9c6.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/easymarket-ui/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "5ff75495463134ddde5cba89df287b9b",
+    "url": "/easymarket-ui/static/media/mobileapp.5ff75495.svg"
+  },
+  {
+    "revision": "b721090e965ea19c66c4307af8915e40",
+    "url": "/easymarket-ui/static/media/mobileapp2.b721090e.svg"
+  },
+  {
+    "revision": "287c074f7ad07edfdfc387a1372ea786",
+    "url": "/easymarket-ui/static/media/network.287c074f.svg"
+  },
+  {
+    "revision": "9bc801319b8e8f50d6f039fb52fcb4fc",
+    "url": "/easymarket-ui/static/media/process.9bc80131.svg"
+  },
+  {
+    "revision": "b74a876104785157e0c9514d54cd49a4",
+    "url": "/easymarket-ui/static/media/security1.b74a8761.svg"
+  },
+  {
+    "revision": "7e7105978025f4938784fc479cdbfc8f",
+    "url": "/easymarket-ui/static/media/security2.7e710597.svg"
+  },
+  {
+    "revision": "29ef607bb72cc9fdd80a24b6f2ec69a5",
+    "url": "/easymarket-ui/static/media/solution.29ef607b.svg"
+  },
+  {
+    "revision": "20837a54dc19f7784d15d06848d74e92",
+    "url": "/easymarket-ui/static/media/trolley.20837a54.svg"
+  }
+]);
